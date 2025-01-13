358290961958a6a42c934608abba85c055c0e362
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2025-01-13 17:10:54 +0100

    Benchmarks D8 WiP

--- benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/benchmarks/animation/AnimatedVisibility.kt
+++ benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/benchmarks/animation/AnimatedVisibility.kt
@@ -1,3 +1,4 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 package benchmarks.animation
 
 import androidx.compose.animation.AnimatedVisibility
@@ -5,36 +6,54 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import compose_benchmarks.benchmarks.generated.resources.Res
 import compose_benchmarks.benchmarks.generated.resources.compose_multiplatform
+import compose_benchmarks.benchmarks.generated.resources.img
+import delays
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.yield
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.LocalResourceReader
+import org.jetbrains.compose.resources.ResourceReader
 import org.jetbrains.compose.resources.painterResource
+import scheduleAfterMyDelay
+
+private var i = 0
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun AnimatedVisibility() {
     MaterialTheme {
+        val res = painterResource(Res.drawable.img)
+//        println("AnimatedVisibility1 ${i++}")
         var showImage by remember { mutableStateOf(false) }
-        LaunchedEffect(showImage) {
-            delay(200)
-            showImage = !showImage
-        }
+//        println("AnimatedVisibility3 ${i++}")
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//            println("AnimatedVisibility4 ${i++}")
             AnimatedVisibility(showImage) {
-                Image(
-                    painterResource(Res.drawable.compose_multiplatform),
-                    null
-                )
+//                Text("AnimatedVisibility")
+//                println("Time ($showImage) =  " + this.transition.playTimeNanos)
+//                println("AnimatedVisibility5 ${i++}")
+//                println("res = $res, ${res.intrinsicSize}")
+                Image(res, null)
             }
         }
+        LaunchedEffect(showImage) {
+//            println("AnimatedVisibilityLE1 ${i++}")
+            myDelay(200)
+//            println("AnimatedVisibilityLE2 ${i++}")
+            showImage = !showImage
+        }
+    }
+}
+
+suspend fun myDelay(time: Long) {
+    suspendCancellableCoroutine {
+        scheduleAfterMyDelay(time, it)
     }
 }
